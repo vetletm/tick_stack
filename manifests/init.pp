76f23e1c0d3b4002a316162cd75ee79960663932
vetletm <vetletm@stud.ntnu.no>
2019-02-18 18:02:15 +0100

    Add order of execution

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,10 @@
 # == Class: tick
 #
 class tick_stack {
-  include tick_stack::telegraf::install
-  -> include tick_stack::telegraf::config
+  class { 'tick_stack::telegraf::install':
+    notify => Class['tick_stack::telegraf::config'],
+  }
+  class { 'tick_stack::telegraf::config':
+    require => Class['tick_stack::telegraf::install'],
+  }
 }
