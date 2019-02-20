0205c0210a174ffac51619e15180fedec853e75f
vetletm <vetletm@stud.ntnu.no>
2019-02-20 14:50:04 +0100

    :lipstick:

--- manifests/telegraf/config.pp
+++ manifests/telegraf/config.pp
@@ -2,32 +2,5 @@ class tick_stack::telegraf::config(
   $defaults = lookup(tick_stack::telegraf::defaults),
   $defaults_telegraf = lookup(tick_stack::telegraf::defaults_telegraf)
   ){
-  # $defaults_telegraf = {
-  #   'ensure'         => present,
-  #   'path'           => '/etc/telegraf/telegraf.conf',
-  #   'section_prefix' => '[',
-  #   'section_suffix' => ']',
-  #   'indent_char'    => ' ',
-  #   'indent_width'   => 2,
-  # }
-
-  # $userpw_telegraf = {
-  #     'global_tags' => {
-  #       'dc' => "\"branch1\""
-  #     },
-  #     'agent' => {
-  #       'interval' => "\"10s\"",
-  #     },
-  #     'outputs.influxdb'  => {
-  #       # Change URL to be DNS resolved hostname, requires whole infrastructure
-  #       'url'       => "\"http://:8086\"",
-  #       'database'  => "\"telegraf\"",
-  #       'precision' => "\"s\""
-  #     },
-  #     'inputs.cpu' => {
-  #       'percpu'    => true,
-  #       'totalcpu'  => true,
-  #     },
-  # }
    create_ini_settings($defaults,$defaults_telegraf)
 }
