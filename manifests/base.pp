fa5c11db5a1fc13dafd440227652e9f28c35c80f
vetletm <vetletm@stud.ntnu.no>
2019-02-20 14:49:43 +0100

    Add base class which adds relevant repo to a node

---
+++ manifests/base.pp
@@ -0,0 +1,28 @@
+# == Class: tick_stack::base
+#
+class tick_stack::base (
+  $osfamily         = $::facts['os']['family'],
+  ){
+
+  if $osfamily == 'Debian' {
+    $operatingsystem  = downcase($::facts['os']['name'])
+    $oscodename       = downcase($::facts['os']['distro']['codename'])
+    apt::source { 'influx':
+      location => "https://repos.influxdata.com/${operatingsystem}",
+      repos    => 'stable',
+      release  => $oscodename,
+      key      => {
+        'id'     => '05CE15085FC09D18E99EFB22684A14CF2582E0C5',
+        'source' => 'https://repos.influxdata.com/influxdb.key',
+      }
+    }
+  } elsif $osfamily == 'RedHat' {
+    yumrepo { 'influx':
+      baseurl  => 'https://repos.influxdata.com/rhel/$releasever/$basearch/stable',
+      descr    => 'InfluxDB Repository - RHEL \$releasever',
+      enabled  => '1',
+      gpgcheck => '1',
+      gpgkey   => 'https://repos.influxdata.com/influxdb.key'
+    }
+  }
+}
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,7 @@
 # == Class: tick
 #
 class tick_stack {
+  include tick_stack::base
   class { 'tick_stack::telegraf::install':
     notify => Class['tick_stack::telegraf::config'],
   }
