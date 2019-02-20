bb120c8134fa03496eb1b1e0dc74f417da6c43bb
vetletm <vetletm@stud.ntnu.no>
2019-02-20 13:57:04 +0100

    Add hiera lookups, remove unused file

--- manifests/telegraf/config.pp
+++ manifests/telegraf/config.pp
@@ -1,4 +1,6 @@
-class tick_stack::telegraf::config {
+class tick_stack::telegraf::config(
+  $defaults = lookup(tick_stack::telegraf::settings)
+  ){
   $defaults_telegraf = {
     'ensure'         => present,
     'path'           => '/etc/telegraf/telegraf.conf',
@@ -8,23 +10,23 @@ class tick_stack::telegraf::config {
     'indent_width'   => 2,
   }
 
-  $userpw_telegraf = {
-      'global_tags' => {
-        'dc' => "\"branch1\""
-      },
-      'agent' => {
-        'interval' => "\"10s\"",
-      },
-      'outputs.influxdb'  => {
-        # Change URL to be DNS resolved hostname, requires whole infrastructure
-        'url'       => "\"http://128.39.169.97:8086\"",
-        'database'  => "\"telegraf\"",
-        'precision' => "\"s\""
-      },
-      'inputs.cpu' => {
-        'percpu'    => true,
-        'totalcpu'  => true,
-      },
-  }
-   create_ini_settings($userpw_telegraf,$defaults_telegraf)
+  # $userpw_telegraf = {
+  #     'global_tags' => {
+  #       'dc' => "\"branch1\""
+  #     },
+  #     'agent' => {
+  #       'interval' => "\"10s\"",
+  #     },
+  #     'outputs.influxdb'  => {
+  #       # Change URL to be DNS resolved hostname, requires whole infrastructure
+  #       'url'       => "\"http://:8086\"",
+  #       'database'  => "\"telegraf\"",
+  #       'precision' => "\"s\""
+  #     },
+  #     'inputs.cpu' => {
+  #       'percpu'    => true,
+  #       'totalcpu'  => true,
+  #     },
+  # }
+   create_ini_settings($defaults,$defaults_telegraf)
 }
