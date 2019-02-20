77ce3307d9ea0b78efa234b69f891b142babc6b4
vetletm <vetletm@stud.ntnu.no>
2019-02-20 15:40:33 +0100

    Change how inifile creates influxdb outputs in config file

--- manifests/telegraf/config.pp
+++ manifests/telegraf/config.pp
@@ -1,6 +1,39 @@
 class tick_stack::telegraf::config(
-  $defaults = lookup(tick_stack::telegraf::defaults),
-  $defaults_telegraf = lookup(tick_stack::telegraf::defaults_telegraf)
-  ){
-   create_ini_settings($defaults,$defaults_telegraf)
+    $defaults = lookup(tick_stack::telegraf::defaults),
+    $defaults_telegraf = lookup(tick_stack::telegraf::defaults_telegraf),
+    $config_path = lookup(tick_stack::telegraf::config_path),
+    $influxdb_url = lookup(tick_stack::telegraf::influxdb_url),
+    $influxdb_database = lookup(tick_stack::telegraf::influxdb_database),
+    $influxdb_precision = lookup(tick_stack::telegraf::influxdb_precision)
+    ){
+
+    create_ini_settings($defaults,$defaults_telegraf)
+
+    ini_setting { 'influxdb':
+      ensure  => present,
+      path => $config_path,
+      section => 'outputs.influxdb',
+      setting => 'url',
+      value   => $influxdb_url,
+      section_prefix => '[',
+      section_suffix => ']',
+    }
+    ini_setting { 'influxdb':
+      ensure  => present,
+      path => $config_path,
+      section => 'outputs.influxdb',
+      setting => 'database',
+      value   => $influxdb_database,
+      section_prefix => '[',
+      section_suffix => ']',
+    }
+    ini_setting { 'influxdb':
+      ensure  => present,
+      path => $config_path,
+      section => 'outputs.influxdb',
+      setting => 'precision',
+      value   => $influxdb_precision,
+      section_prefix => '[',
+      section_suffix => ']',
+    }
 }
