a26d7981dac7342e9a7fe534f7d013865e873b22
vetletm <vetletm@stud.ntnu.no>
2019-02-20 15:07:57 +0100

    :lipstick:

--- manifests/telegraf/install.pp
+++ manifests/telegraf/install.pp
@@ -1,30 +1,5 @@
 class tick_stack::telegraf::install {
-  # $osfamily         = $::facts['os']['family'],
-  # ){
-  #
-  # if $osfamily == 'Debian' {
-  #   $operatingsystem  = downcase($::facts['os']['name'])
-  #   $oscodename       = downcase($::facts['os']['distro']['codename'])
-  #   apt::source { 'influx':
-  #     location => "https://repos.influxdata.com/${operatingsystem}",
-  #     repos    => 'stable',
-  #     release  => $oscodename,
-  #     key      => {
-  #       'id'     => '05CE15085FC09D18E99EFB22684A14CF2582E0C5',
-  #       'source' => 'https://repos.influxdata.com/influxdb.key',
-  #     },
-  #     before   => Package['telegraf'],
-  #   }
-  # } elsif $osfamily == 'RedHat' {
-  #   yumrepo { 'influx':
-  #     baseurl  => 'https://repos.influxdata.com/rhel/$releasever/$basearch/stable',
-  #     descr    => 'InfluxDB Repository - RHEL \$releasever',
-  #     enabled  => '1',
-  #     gpgcheck => '1',
-  #     gpgkey   => 'https://repos.influxdata.com/influxdb.key',
-  #     before   => Package['telegraf'],
-  #   }
-  # }
+
   package { 'telegraf':
     ensure => latest,
     notify => Service['telegraf'],
