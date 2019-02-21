7d5182a2d0b1d02b092a834cbe9fa18357137963
vetletm <vetletm@stud.ntnu.no>
2019-02-21 13:55:42 +0100

    Change how conditionals are checked...

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,18 +4,18 @@ class tick_stack (
   String $influxdb_url,
   String $influxdb_database,
   String $influxdb_precision,
+  String $include_telegraf,
+  String $include_influxdb,
 
-  Bool $include_telegraf,
-  Bool $include_influxdb,
   ){
   contain tick_stack::influxdb
   contain tick_stack::telegraf
 
   Class['tick_stack::base'] ->
-  if $include_influxdb {
+  if $include_influxdb == 'True' {
     include tick_stack::influxdb
   }
-  if $include_telegraf {
+  if $include_telegraf == 'True' {
     include tick_stack::telegraf
   }
 }
