e2a9c18633968057c89adf200ef0dfbabe76196c
vetletm <vetletm@stud.ntnu.no>
2019-02-21 11:57:05 +0100

    Trying something fancy

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,7 @@
 #
 class tick_stack {
   class { 'tick_stack::base':
-    before => Class['tick_stack::telegraf::install']
+    before => Class['tick_stack::telegraf::install', 'tick_stack::influxdb']
   }
 
   class { 'tick_stack::telegraf::install':
@@ -13,6 +13,5 @@ class tick_stack {
   }
 
   class { 'tick_stack::influxdb':
-    require => Class['tick_stack::base']
   }
 }
