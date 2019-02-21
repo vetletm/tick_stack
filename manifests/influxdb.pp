9d84efa4b38d915a813ef068af9711574f4a8896
vetletm <vetletm@stud.ntnu.no>
2019-02-21 14:05:42 +0100

    Got weird error message, trying whatever

--- manifests/influxdb.pp
+++ manifests/influxdb.pp
@@ -1,5 +1,8 @@
 # == Class: tick_stack::influxdb
 #
 class tick_stack::influxdb {
+  contain tick_stack::influxdb::install
+  contain tick_stack::influxdb::config
+  
   Class['tick_stack::influxdb::install'] -> Class['tick_stack::influxdb::config']
 }
--- manifests/telegraf.pp
+++ manifests/telegraf.pp
@@ -1,5 +1,8 @@
 # == Class: tick_stack::telegraf
 #
 class tick_stack::telegraf {
+  contain tick_stack::telegraf::install
+  contain tick_stack::telegraf::config
+
   Class['tick_stack::telegraf::install'] -> Class['tick_stack::telegraf::config']
 }
