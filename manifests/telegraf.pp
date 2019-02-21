4ee8b1094fb0c5f59c3ab055ac6e62e8a924d580
vetletm <vetletm@stud.ntnu.no>
2019-02-21 15:20:35 +0100

    Remove include

--- manifests/telegraf.pp
+++ manifests/telegraf.pp
@@ -5,9 +5,6 @@ class tick_stack::telegraf(
   String $influxdb_database,
   String $influxdb_precision,
   ){
-  include tick_stack::telegraf::install
-  include tick_stack::telegraf::config
-
   class {'tick_stack::telegraf::install':
     require => Class['tick_stack::repo']
   } -> Class['tick_stack::telegraf::config']
