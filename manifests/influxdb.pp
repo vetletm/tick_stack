0a230cceec741970ddc9b0352283914f55ba3547
vetletm <vetletm@stud.ntnu.no>
2019-02-21 15:21:14 +0100

    Change ordering in influxdb

--- manifests/influxdb.pp
+++ manifests/influxdb.pp
@@ -1,9 +1,6 @@
 # == Class: tick_stack::influxdb
 #
 class tick_stack::influxdb {
-  contain tick_stack::influxdb::install
-  contain tick_stack::influxdb::config
-
   class {'tick_stack::influxdb::install':
     require => Class['tick_stack::repo']
   } -> Class['tick_stack::influxdb::config']
