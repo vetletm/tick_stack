bf2d237f964f4ecad554d3c5bf93696cb80c7edf
vetletm <vetletm@stud.ntnu.no>
2019-02-21 15:55:41 +0100

    Add include repo to all classes that need it

--- manifests/influxdb.pp
+++ manifests/influxdb.pp
@@ -1,6 +1,7 @@
 # == Class: tick_stack::influxdb
 #
 class tick_stack::influxdb {
+  include tick_stack::repo
   class {'tick_stack::influxdb::install':
     require => Class['tick_stack::repo']
   }
--- manifests/telegraf.pp
+++ manifests/telegraf.pp
@@ -5,6 +5,7 @@ class tick_stack::telegraf(
   String $influxdb_database,
   String $influxdb_precision,
   ){
+  include tick_stack::repo
   class {'tick_stack::telegraf::install':
     require => Class['tick_stack::repo']
   }
