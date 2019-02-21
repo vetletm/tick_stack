af9f96928243820d1aa2048cb590363630c0d9f9
vetletm <vetletm@stud.ntnu.no>
2019-02-21 14:18:58 +0100

    Idea didn't work, making strictly monolithic to a later date.

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,24 +4,13 @@ class tick_stack (
   String $influxdb_url,
   String $influxdb_database,
   String $influxdb_precision,
-  String $include_telegraf,
-  String $include_influxdb,
 
   ){
   contain tick_stack::base
   contain tick_stack::influxdb
   contain tick_stack::telegraf
 
-  include tick_stack::base
-
-  if $include_influxdb == 'True' {
-    class {'tick_stack::influxdb':
-      require => Class['tick_stack::base']
-    }
-  }
-  if $include_telegraf == 'True' {
-    class {'tick_stack::telegraf':
-      require => Class['tick_stack::base']
-    }
-  }
+  Class['tick_stack::base']
+  -> Class['tick_stack::influxdb']
+  -> Class['tick_stack::telegraf']
 }
