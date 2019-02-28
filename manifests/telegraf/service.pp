8364b2cfc4903e878366f8c1a07ed861a15d3de5
vetletm <vetletm@stud.ntnu.no>
2019-02-28 19:34:23 +0100

    Change service restart to 'hasrestart' because it wouldnt restart.

--- manifests/telegraf/service.pp
+++ manifests/telegraf/service.pp
@@ -4,8 +4,8 @@ class tick_stack::telegraf::service (
   $service = $tick_stack::telegraf::params::service
   ) inherits tick_stack::telegraf::params {
   service { 'telegraf':
-    ensure  => $service,
-    enable  => true,
-    restart => true,
+    ensure     => $service,
+    enable     => true,
+    hasrestart => true,
   }
 }
