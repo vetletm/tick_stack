d671946ed7b3f941dd4fc111535f72ef2d3dbf07
vetletm <vetlemoen@gmail.com>
2019-05-02 10:56:56 +0200

    Update kapacitor template to use scopes and clean up code

--- manifests/kapacitor.pp
+++ manifests/kapacitor.pp
@@ -3,7 +3,8 @@
 class tick_stack::kapacitor (
   $ensure     = $tick_stack::kapacitor::params::ensure,
 
-  $path       = $tick_stack::kapacitor::params::path,
+  $conf_path  = $tick_stack::kapacitor::params::conf_path,
+  $template   = $tick_stack::kapacitor::params::template,
 
   $service    = $tick_stack::kapacitor::params::service,
   $enable     = $tick_stack::kapacitor::params::enable,
@@ -19,7 +20,8 @@ class tick_stack::kapacitor (
     ensure  => $ensure,
   }
   -> class {'tick_stack::kapacitor::config':
-    path   => $path,
+    conf_path => $conf_path,
+    template  => $template,
   }
   ~> class {'tick_stack::kapacitor::service':
     service    => $service,
